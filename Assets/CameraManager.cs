60444f6ed6cb85ddecf63fbb1dabcc7a56929414
c-aranda-bassegoda <carolina.aranda.2699@gmail.com>
2025-09-23 15:08:30 +0200

    camera rotation controls

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -1,18 +1,56 @@
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class CameraManager : MonoBehaviour
 {
+    InputManager inputManager;
+
     public Transform targetTransform;
+    public Transform cameraPivot;
     private Vector3 cameraVelocity = Vector3.zero;
+
     public float cameraSpeed = 0.2f;
+    public float cameraTurnSpeed = 2f;
+
+    public float lookAngle;
+    public float pivotAngle;
+    public float minPivot = -35;
+    public float maxPivot = 35;
 
     private void Awake()
     {
+        inputManager = FindFirstObjectByType<InputManager>();
         targetTransform = FindFirstObjectByType<PlayerManager>().transform;
     }
-    public void FollowTarget()
+
+    public void HandleCameraMovement()
+    {
+        FollowTarget();
+        RotateCamera();
+    }
+
+    private void FollowTarget()
     {
         Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraVelocity, cameraSpeed);
         transform.position = targetPosition;
+    }
+
+    private void RotateCamera()
+    {
+        lookAngle += (inputManager.cameraInputX * cameraTurnSpeed);
+        pivotAngle -= (inputManager.cameraInputY * cameraTurnSpeed);
+        pivotAngle = Mathf.Clamp(pivotAngle, minPivot, maxPivot);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,6 +4,10 @@ public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
     public Vector2 movementInput;
+    public Vector2 cameraInput;
+
+    public float cameraInputX;
+    public float cameraInputY;
     public float VerticalInput { get; set; }
     public float HorizontalInput { get; set; }
 
@@ -14,6 +18,7 @@ public class InputManager : MonoBehaviour
             playerControls = new PlayerControls();
 
             playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            playerControls.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
 
         }
 
@@ -35,5 +40,8 @@ public class InputManager : MonoBehaviour
     {
         VerticalInput = movementInput.y;
         HorizontalInput = movementInput.x;
+
+        cameraInputX = cameraInput.x;
+        cameraInputY = cameraInput.y;
     }
 }
